dd5029f4d24a571748fe9a040e686da9213001f5
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-06-26 23:24:12 +0700

    fixing assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,78 +1,76 @@
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- <title>{{ config('app.name', 'Laravel 8 User Roles and Permissions Tutorial') }}</title> -->
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> -->
-    <!-- Styles -->
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- <title>{{ config('app.name', 'Laravel 8 User Roles and Permissions Tutorial') }}</title> -->
+        
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css"> -->
+        <!-- Styles -->
+        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
 
-    <!-- Favicon -->
-    <link rel="icon" href="{{ asset('assets/img/brand/favicon.png') }}" type="image/png">
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-    <!-- Icons -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
-    <!-- Page plugins -->
-    <!-- Argon CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/argon.css?v=1.2.0') }}" type="text/css">
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('assets/img/brand/favicon.png') }}" type="image/png">
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+        <!-- Icons -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+        <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+        <!-- Page plugins -->
+        <!-- Argon CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/css/argon.css?v=1.2.0') }}" type="text/css">
 
-</head>
-<body>
-    <div id="app">
+    </head>
+    <body>
+        <div id="app">
             @auth
                 @include('layouts.sidenav')
             @endauth
 
-        <!-- Main content -->
-        <div class="main-content" id="panel">
-            @auth
-                @include('layouts.topnav')
-            @endauth
+            <!-- Main content -->
+            <div class="main-content" id="panel">
+                @auth
+                    @include('layouts.topnav')
+                @endauth
 
-            <div class="container-fluid mt-6">
-            
-                @yield('content')
+                <div class="container-fluid mt-6">
+                    @yield('content')
+                </div>
 
+                @auth
+                    @include('layouts.footer')
+                @endauth
             </div>
 
-            @auth
-                @include('layouts.footer')
-            @endauth
+            <!-- Argon Scripts -->
+            <!-- Core -->
+            <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+            <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
+            <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+            <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+
+            <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+            <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+            <script src="{{ asset('assets/vendor/js-cookie/js.cookie.js') }}"></script>
+            <script src="{{ asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+            <script src="{{ asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>  
+
+            <!-- Optional JS -->
+            <script src="{{ asset('assets/vendor/chart.js/dist/Chart.min.js') }}"></script>
+            <script src="{{ asset('assets/vendor/chart.js/dist/Chart.extension.js') }}"></script>
+
+            <!-- Argon JS -->
+            {{-- <script src="../assets/js/argon.js?v=1.2.0"></script> --}}
+            {{-- <script src="{{ asset('assets/js/argon.js?v=1.2.0') }}"></script> --}}
         </div>
-
-        <!-- Argon Scripts -->
-        <!-- Core -->
-        <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
-        <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
-        <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-        <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
-
-        <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/js-cookie/js.cookie.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
-        <!-- Optional JS -->
-        <!-- <script src="../assets/vendor/chart.js/dist/Chart.min.js"></script>
-        <script src="../assets/vendor/chart.js/dist/Chart.extension.js"></script> -->
-
-        <script src="{{ asset('assets/vendor/chart.js/dist/Chart.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/chart.js/dist/Chart.extension.js') }}"></script>
-        <!-- Argon JS -->
-        <!-- <script src="../assets/js/argon.js?v=1.2.0"></script> -->
-
-        <script src="{{ asset('assets/js/argon.js?v=1.2.0') }}"></script>
-    </div>
-</body>
+    </body>
 </html>
